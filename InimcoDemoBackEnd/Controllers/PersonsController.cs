a59858cf09f62a03fa7cead1c56b52b8b0496238
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-05 00:50:49 +0100

    renaming the person control

--- InimcoDemoBackEnd/Controllers/PersonsController.cs
+++ InimcoDemoBackEnd/Controllers/PersonsController.cs
@@ -5,13 +5,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace InimcoDemoBackEnd.Controllers
 {
     [ApiController]
-    [Route("[controller]/persons")]
-    public class PersonController : ControllerBase
+    [Route("[controller]")]
+    public class PersonsController : ControllerBase
     {
-        private readonly ILogger<PersonController> _logger;
+        private readonly ILogger<PersonsController> _logger;
         private readonly IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        public PersonsController(ILogger<PersonsController> logger, IPersonService personService)
         {
             _logger = logger;
             _personService = personService;
@@ -26,6 +26,23 @@ namespace InimcoDemoBackEnd.Controllers
             {
                 var insertedPerson = await _personService.InsertNewPerson(person);
                 return Ok(insertedPerson);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        //* Extended person calls
+        //TODO: change to uint (need to build a custom route constraint)
+        [HttpGet("extended/{id}", Name = "GetExtendedPerson")]
+        public async Task<ActionResult> GetExtendedPerson(uint id)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                var requestedPerson = await _personService.GetExtendedPersonById(id);
+                return Ok(requestedPerson);
             }
             catch
             {
