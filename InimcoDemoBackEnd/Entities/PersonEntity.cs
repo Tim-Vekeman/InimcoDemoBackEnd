1ef2c4520737c6a800a8af464bf3d3e21b3ef53d
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-04 16:48:36 +0100

    fixing erro in migration and migration

--- InimcoDemoBackEnd/Entities/PersonEntity.cs
+++ InimcoDemoBackEnd/Entities/PersonEntity.cs
@@ -8,7 +8,7 @@ namespace InimcoDemoBackEnd.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public uint Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string[] SocialSkills { get; set; }
---
+++ InimcoDemoBackEnd/Migrations/20231204154813_init.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace InimcoDemoBackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Firstname = table.Column<string>(type: "text", nullable: false),
+                    Lastname = table.Column<string>(type: "text", nullable: false),
+                    SocialSkills = table.Column<string[]>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMediaAccountEntity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PersonEntityId = table.Column<long>(type: "bigint", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMediaAccountEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SocialMediaAccountEntity_Persons_PersonEntityId",
+                        column: x => x.PersonEntityId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMediaAccountEntity_PersonEntityId",
+                table: "SocialMediaAccountEntity",
+                column: "PersonEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SocialMediaAccountEntity");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
