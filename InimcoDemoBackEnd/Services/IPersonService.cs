8f58616749aae322551a34ee8e72028129a11062
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-04 21:57:32 +0100

    Working out the post request logic in the service

--- InimcoDemoBackEnd/Services/IPersonService.cs
+++ InimcoDemoBackEnd/Services/IPersonService.cs
@@ -1,6 +1,14 @@
-﻿namespace InimcoDemoBackEnd.Services
+﻿using InimcoDemoBackEnd.Models;
+
+namespace InimcoDemoBackEnd.Services
 {
     public interface IPersonService
     {
+        /// <summary>
+        /// Async function to insert a new Person into the database
+        /// </summary>
+        /// <param name="personToInsert">The dto containing the data of the person to insert</param>
+        /// <returns>A personDto containg the data of the inserted user</returns>
+        public Task<PersonDto> InsertNewPerson(PersonDto personToInsert);
     }
 }
--- InimcoDemoBackEnd/Services/PersonService.cs
+++ InimcoDemoBackEnd/Services/PersonService.cs
@@ -1,6 +1,32 @@
-﻿namespace InimcoDemoBackEnd.Services
+﻿using InimcoDemoBackEnd.DatabaseContext;
+using InimcoDemoBackEnd.Entities;
+using InimcoDemoBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace InimcoDemoBackEnd.Services
 {
     public class PersonService : IPersonService
     {
+        private readonly PersonDatabaseContext personDatabaseContext;
+
+        public PersonService(PersonDatabaseContext personDatabaseContext) 
+        {
+            this.personDatabaseContext = personDatabaseContext;
+        }
+
+        public async Task<PersonDto> InsertNewPerson(PersonDto personToInsert)
+        {
+            // Create a new User entity
+            var person = new PersonEntity(personToInsert);
+
+            // Add the user to the database
+            personDatabaseContext.Persons.Add(person);
+
+            // Save changes to the database
+            await personDatabaseContext.SaveChangesAsync();
+
+            return new PersonDto(person);
+        }
     }
 }
