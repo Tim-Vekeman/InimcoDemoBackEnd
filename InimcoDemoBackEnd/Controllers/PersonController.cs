0a1453fa2f532862966bf074be9e7efd71d47656
Tim Vekeman <tim.vekeman@student.odisee.be>
2023-12-04 21:59:14 +0100

    Working out the post request for the person

--- InimcoDemoBackEnd/Controllers/PersonController.cs
+++ InimcoDemoBackEnd/Controllers/PersonController.cs
@@ -1,23 +1,36 @@
 using InimcoDemoBackEnd.Models;
+using InimcoDemoBackEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InimcoDemoBackEnd.Controllers
 {
     [ApiController]
-    [Route("[controller]/Person")]
+    [Route("[controller]/persons")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
+        private readonly IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
         [HttpPost(Name = "AddPerson")]
         public async Task<ActionResult> AddPerson([FromBody] PersonDto person)
         {
-            return Ok();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!person.IsValid(false)) return BadRequest("Data isn't valid");
+            try
+            {
+                var insertedPerson = await _personService.InsertNewPerson(person);
+                return Ok(insertedPerson);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
